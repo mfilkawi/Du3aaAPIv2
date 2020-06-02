5e581f394ae511977771ac9fcae848e09d75a836
mfilkawi <m.filkawi@gmail.com>
2020-06-03 02:43:15 +0300

    styling changed for sign in btn and err msg

--- public/javascript/getTwitterOAuthURL.js
+++ public/javascript/getTwitterOAuthURL.js
@@ -5,10 +5,11 @@ fetch(`${window.location.origin}/twitter/oauth`,{
     .then(data => {
         if(data.status == 404) throw err;
         const SignInContainer = document.querySelector('.sign-in-btn');
-        SignInContainer.innerHTML = `<a href="${data.url}"><img src="./style/img/sign-in-with-twitter-gray.png" alt="sign in with twitter"></a>`;
+        // SignInContainer.innerHTML = `<a href="${data.url}"><img src="./style/img/sign-in-with-twitter-gray.png" alt="sign in with twitter"></a>`;
+        SignInContainer.innerHTML = `<a href="${data.url}" id="signin">اضغط هنا للإشتراك</a>`;
     })
     .catch(err => {
         console.log(err);
         const SignInContainer = document.querySelector('.sign-in-btn');
-        SignInContainer.innerHTML = "الرجاء المحاولة لاحقاً";
+        SignInContainer.innerHTML = `<span style="color:red; font-weight: 600;">الرجاء المحاولة لاحقاً</span>`;
     })
